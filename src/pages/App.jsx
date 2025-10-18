65c050bb3bd4174ccc7f8009b06734904c0deaed
Ashar A <133260804+SoccerFalcon@users.noreply.github.com>
2025-10-18 16:27:44 -0700

    my version of audio

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,22 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import AudioRecorder from './AudioRecorder';
 
 function App() {
-  const [toUse, setToUse] = useState("Welcome to our React app, blud");
-
-  const recordOn = () => {
-    setToUse(toUse + " hi");
-  };
+  const [audio, setAudio] = useState(null);
 
   return (
     <>
-<<<<<<< HEAD
       <h1>Click the record button below to start recording</h1>
-      <button>Record</button>
-=======
-      <h1>{toUse}</h1>
-      <button onClick={recordOn}>Record</button>
->>>>>>> e356d0d4a3e1761ace2e1570333ca49403137c43
+      <AudioRecorder setAudioCallback={setAudio}/>
     </>
   );
 }
